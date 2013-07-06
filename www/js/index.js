b22cb95f19d27b79a8f8c8ee91abe56984103b40
Daan Debie <daan.debie@info.nl>
2013-07-06 20:52:09 +0200

    Login working

--- www/js/index.js
+++ www/js/index.js
@@ -21,36 +21,48 @@ var app = {
             logging: true
         });
 
-        client.login("daan", "buza", function (error, response) {
-             if (error) { 
-                console.log("login failed");
-             } else { 
-                console.log("login succeeded"); 
-            } 
-         });
+        $('.inloggen').on('click', function(){
+            var password = $('.password-field').val(),
+                email = $('.email-field').val();
+            client.login(email, password, function (error, response) {
+                if (error) { 
+                   alert('De gegevens die u heeft ingevoerd zijn niet bekend bij ons.');
+                } else { 
+                   location.href = 'home.html' 
+                } 
+            });
+        });
 
-        var journeys = new Usergrid.Collection({ "client":client, "type":"journeys" });
-
-        var journey = {"destination": "Bla" + Math.random()};
-        journeys.addEntity(journey, function (error, response) {
-             if (error) { 
-                console.log("write failed");
-             } else { 
-                console.log("write succeeded"); 
-            } 
-         });
-        console.log(journey);
-        
-        journeys.fetch(
-            function() { // Success
-                while(journeys.hasNextEntity()) {
-                    var journey = journeys.getNextEntity();
-                    console.log(journey); // Output the title of the book
-                }
-            }, function() { // Failure
-                console.log("read failed");
+        $('.password-field').keyup(function(event){
+            if(event.keyCode == 13){
+                $('.inloggen').click();
             }
-        );      
+        });
+
+        
+
+        // var journeys = new Usergrid.Collection({ "client":client, "type":"journeys" });
+
+        // var journey = {"destination": "Bla" + Math.random()};
+        // journeys.addEntity(journey, function (error, response) {
+        //      if (error) { 
+        //         console.log("write failed");
+        //      } else { 
+        //         console.log("write succeeded"); 
+        //     } 
+        //  });
+        // console.log(journey);
+        
+        // journeys.fetch(
+        //     function() { // Success
+        //         while(journeys.hasNextEntity()) {
+        //             var journey = journeys.getNextEntity();
+        //             console.log(journey); // Output the title of the book
+        //         }
+        //     }, function() { // Failure
+        //         console.log("read failed");
+        //     }
+        // );      
     },
 };
 
